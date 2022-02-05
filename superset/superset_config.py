8a157d8446780e4e71550405cbedde8a4d64d92a
akashkj <akashj1708@gmail.com>
2022-02-05 21:52:51 +0530

    Create superset_config.py

---
+++ superset/superset_config.py
@@ -0,0 +1,9 @@
+# ----------------------------------------------------
+# AUTHENTICATION CONFIG
+# ----------------------------------------------------
+# The authentication type
+# AUTH_OID : Is for OpenID
+# AUTH_DB : Is for database (username/password)
+# AUTH_LDAP : Is for LDAP
+# AUTH_REMOTE_USER : Is for using REMOTE_USER from web server
+AUTH_TYPE = AUTH_OID
